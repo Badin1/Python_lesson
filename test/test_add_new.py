2c20b03225ca9cfcad31816f1ad29a0708096c40
Badin1 <66058511+Badin1@users.noreply.github.com>
2020-12-18 23:22:04 +0300

    Задание №6: Сделать фикстуру общей для всех тестов

--- test/test_add_new.py
+++ test/test_add_new.py
@@ -1,16 +1,8 @@
 # -*- coding: utf-8 -*-
-import pytest
 from model.add_new import Add_New
-from fixture.application_new import ApplicationNew
 
-@pytest.fixture
-def appn(request):
-    fixture = ApplicationNew()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
-def test_add_new(appn):
-    appn.session_new.login(username="admin", password="secret")
-    appn.add_new.create(Add_New(firstname="Petrov", lastname="Petr", address="Moscow", telephone="8-905-323-50-26", email="petrov@yandex.ru"))
-    appn.session_new.logout()
-
+def test_add_new(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.create_new(Add_New(firstname="Petrov", lastname="Petr", address="Moscow", telephone="8-905-323-50-26", email="petrov@yandex.ru"))
+    app.session.logout()
